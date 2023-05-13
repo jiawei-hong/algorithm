012a52eb65946f96e65e37e31eaaeeace15a080b
Wei Hong <zxc37984@gmail.com>
2023-05-14 01:08:33 +0800

    :sparkles: Memoize

---
+++ 30-days-of-LC-javascript-challenge/memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+  let cache = new Map()
+  return function(...args) {
+      let key = JSON.stringify(args)
+
+      if(cache.has(key)) return cache.get(key)
+
+      const output = fn(...args)
+      cache.set(key,output)
+
+      return output
+  }
+}
+
+
+/** 
+* let callCount = 0;
+* const memoizedFn = memoize(function (a, b) {
+*	 callCount += 1;
+*   return a + b;
+* })
+* memoizedFn(2, 3) // 5
+* memoizedFn(2, 3) // 5
+* console.log(callCount) // 1 
+*/
