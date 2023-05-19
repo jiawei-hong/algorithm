7d09d1b157184e46a8e73616a030b3ddb036cade
Wei Hong <zxc37984@gmail.com>
2023-05-19 20:17:37 +0800

    :sparkles: Deboune

---
+++ 30-days-of-LC-javascript-challenge/deboune.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+  let timeout =  null;
+  return function(...args) {
+      if(timeout){
+          clearTimeout(timeout)
+      }
+      timeout = setTimeout(() => fn(...args), t) 
+  }
+};
+
+/**
+* const log = debounce(console.log, 100);
+* log('Hello'); // cancelled
+* log('Hello'); // cancelled
+* log('Hello'); // Logged at t=100ms
+*/
