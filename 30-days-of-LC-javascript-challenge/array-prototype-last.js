ca89d42a415c2f9b0334d5343a7ea786b07bbd31
Wei Hong <zxc37984@gmail.com>
2023-05-28 01:51:05 +0800

    :sparkles: Array Prototype Last

---
+++ 30-days-of-LC-javascript-challenge/array-prototype-last.js
@@ -0,0 +1,14 @@
+Array.prototype.last = function() {
+  const n = this.length;
+
+  if (n == 0) {
+      return -1
+  }
+
+  return this[n-1];
+};
+
+/**
+* const arr = [1, 2, 3];
+* arr.last(); // 3
+*/
