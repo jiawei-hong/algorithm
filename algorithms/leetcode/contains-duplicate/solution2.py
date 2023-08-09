c1e47872c949c97d05199b3f701c3af9fdf7e01c
Wei Hong <zxc37984@gmail.com>
2023-08-09 21:26:16 +0800

    [0217] Contains Duplicate

---
+++ algorithms/leetcode/contains-duplicate/solution2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+
+        for num in nums:
+            if num in seen and seen.get(num) == 1:
+                return True
+
+            seen[num] = seen.get(num, 0) + 1
+
+        return False
