de703b5ddf3ddf6ed0870ee1b7c3ac1fc2d0228e
Wei Hong <zxc37984@gmail.com>
2024-03-17 13:55:28 +0800

    [57] Insert Interval

---
+++ algorithms/leetcode/insert-interval/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        i = 0
+        res = []
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval = [
+                min(intervals[i][0], newInterval[0]),
+                max(intervals[i][1], newInterval[1]),
+            ]
+            i += 1
+        res.append(newInterval)
+
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
