5e064cf4f3dfecc787e60d9c7c036954898c0910
Wei Hong <zxc37984@gmail.com>
2024-06-11 06:53:19 +0800

    [Hacker Rank] Repeated String

---
+++ hacker-rank/repeated-string.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    counting_a_in_s = s.count('a')
+    cycle = n // len(s)
+    remainder_n = n % len(s)
+    counting_a_remainder = s[:remainder_n].count('a')
+
+    return counting_a_in_s * cycle + counting_a_remainder
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
