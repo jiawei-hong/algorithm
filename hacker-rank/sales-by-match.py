04356bda694f9ae7f3a4a143e14dcd2665e386e6
Wei Hong <zxc37984@gmail.com>
2024-06-10 21:57:04 +0800

    [Hacker Rank] Sales by Match

---
+++ hacker-rank/sales-by-match.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    count = {}
+    res = 0
+    print(ar)
+    for num in ar:
+        count[num] = count.get(num, 0) + 1
+
+        if count[num] % 2 == 0:
+            res += 1
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
